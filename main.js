fd39411dc114ef22a66478dd79b3693de693ea81
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2022-05-23 09:18:48 +0200

    hamburger

--- main.js
+++ main.js
@@ -5,5 +5,10 @@ window.addEventListener(`load`, () => {
         const ifr = document.getElementById(`ifr`);
         ifr.src = `./frontPage.html`;
     });
+    const openMenu = document.getElementById(`showMenu`);
+    openMenu.addEventListener(`click`, () => {
+        const menu = document.getElementById(`menuItems`);
+        menu.classList.contains(`menuItems_hidden`) ? menu.classList.remove(`menuItems_hidden`) : menu.classList.add(`menuItems_hidden`);
+    });
 });
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -4,4 +4,9 @@ window.addEventListener(`load`, () => {
 		const ifr = document.getElementById(`ifr`) as HTMLIFrameElement;
 		ifr.src = `./frontPage.html`;
 	});
+	const openMenu = document.getElementById(`showMenu`) as HTMLButtonElement;
+	openMenu.addEventListener(`click`, () => {
+		const menu = document.getElementById(`menuItems`) as HTMLUListElement;
+		menu.classList.contains(`menuItems_hidden`)? menu.classList.remove(`menuItems_hidden`) : menu.classList.add(`menuItems_hidden`);
+	});
 });
